b738f883f49280f2f30d8f5ed13237f21480d954
Denissimo <droden@narod.ru>
2018-04-03 00:01:11 +0300

    save0402

--- Assets/Script/Portal.cs
+++ Assets/Script/Portal.cs
@@ -11,6 +11,7 @@ public class Portal : MonoBehaviour {
 	public GameObject portal1;
 	public GameObject portal2;
 	public GameObject rotatedPortal;
+	public GameObject baida;
 
 	public GameObject cube;
 
@@ -18,22 +19,36 @@ public class Portal : MonoBehaviour {
 	public Camera camera2;
 	public Quaternion quatPortal = Quaternion.Euler(-90, 0, 180);
 	public Quaternion quatCamera = Quaternion.Euler(90, 0, 0);
+	public Quaternion quatBaidaX;
+	public Quaternion quatBaidaY;
+	public Quaternion quatBaidaZ;
+	public Quaternion baidaRot;
 
 	float degPerTime;
 
-
+	float baiX = .3F;
+	float baiY = .3F;
+	float baiZ = .3F;
 
 	// Use this for initialization
 	void Start () {
 		//transform.position = new Vector3 (100, 100, 100);
 		Debug.Log ("ZZZZZ");
+		baidaRot = baida.transform.rotation;
 
 		//Ray ray = new Ray (portal1.transform.position, 2);
 
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+
+
+        quatBaidaX = Quaternion.AngleAxis(baiX++, Vector3.right);
+        quatBaidaY = Quaternion.AngleAxis(baiY++, Vector3.up);
+        quatBaidaZ = Quaternion.AngleAxis(baiZ++, Vector3.forward);
+
+        baida.transform.rotation = baidaRot * quatBaidaX * quatBaidaY * quatBaidaZ;
 
 		rot ();
 
@@ -56,19 +71,22 @@ public class Portal : MonoBehaviour {
 		camera1.transform.position = (portalPosition2 + portalRotation2 * distance1);
 		camera2.transform.position = (portalPosition1 + portalRotation1 * distance2);
 
+		camera1.transform.rotation = playerRotation;
+		camera1.transform.Rotate(0, 180, 0);
+
 		camera2.transform.rotation = playerRotation;
-		camera2.transform.Rotate(180, 0, 180);
+		camera2.transform.Rotate(0, 180, 0);
 
 		//camera1.transform.rotation
 
-		camera1.transform.LookAt (portal2.transform.position);
+		//camera1.transform.LookAt (portal2.transform.position);
 		//camera2.transform.LookAt (portal1.transform.position);
 		//Debug.Log (distance2);
 	}
 
 	void rot()
 	{
-		float rotPerSec = .00F;
+		float rotPerSec = .01F;
 		float degPerSec = 360 * rotPerSec;
 		degPerTime = degPerSec * Time.deltaTime;
 

15425e56f8256344acd0f8dd30817df29bcfd65d
Denissimo <droden@narod.ru>
2018-07-21 20:26:47 +0300

    Smooth teleport

--- Assets/Script/Send.cs
+++ Assets/Script/Send.cs
@@ -11,13 +11,31 @@ public class Send : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		Vector3 playerPosition = player.transform.position;
+		Vector3 otherPortalPosition = otherPortal.transform.position;
+		Vector3 portalPosition = portal.transform.position;
+		Debug.Log(
+			"portalPosition: x = " + portalPosition.x +
+			",y = " + portalPosition.y + 
+			",z = " + portalPosition.z 
+		);
+
+		Debug.Log(
+			"otherPortalPosition: x = " + otherPortalPosition.x +
+			",y = " + otherPortalPosition.y + 
+			",z = " + otherPortalPosition.z 
+		);
 	}
 
 	// Update is called once per frame
 	void Update () {
-
-
+		if (Input.GetKeyDown (KeyCode.Mouse0)) {
+			Debug.Log(
+				"Player: x = " + player.transform.position.x +
+				",y = " + player.transform.position.y + 
+				",z = " + player.transform.position.z 
+			);
+		}
 	}
 
 	void OnTriggerEnter (Collider portalCollider) {
@@ -25,17 +43,47 @@ public class Send : MonoBehaviour {
 		Vector3 playerPosition = player.transform.position;
 		Vector3 otherPortalPosition = otherPortal.transform.position;
 		Vector3 portalPosition = portal.transform.position;
+		Vector3 colliderPosition = portalCollider.transform.position;
 
 		Quaternion playerRotation = player.transform.rotation;
 		Quaternion otherPortalRotation = otherPortal.transform.rotation;
 		Quaternion portalRotation = this.transform.rotation;
 
 
-		Vector3 distance = portalPosition - playerPosition;
+		//Vector3 distance = colliderPosition - playerPosition;
+		Vector3 distance = playerPosition - portalPosition;
+
+		Debug.Log(
+			"distance: x = " + distance.x +
+			",y = " + distance.y + 
+			",z = " + distance.z 
+		);
+
 		Vector3 newPosition = otherPortalPosition + distance;
 
 		player.transform.position = newPosition;
-		Debug.Log(player.transform.position.x);
+
+		Debug.Log(
+			"Portal: x = " + portalPosition.x +
+			",y = " + portalPosition.y + 
+			",z = " + portalPosition.z 
+		);
+
+
+		Debug.Log(
+			"Collider: x = " + colliderPosition.x +
+			",y = " + colliderPosition.y + 
+			",z = " + colliderPosition.z 
+		);
+
+
+		Debug.Log(
+			"Player: x = " + playerPosition.x +
+			",y = " + playerPosition.y + 
+			",z = " + playerPosition.z 
+		);
+
+
 
 	}
 }
